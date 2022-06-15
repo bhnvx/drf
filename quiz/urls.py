66dec5cdb22675ddaa33e14d9cc253c4387ec035
KyoungHwan <xzt______@naver.com>
2022-06-15 11:02:57 +0900

    New Setting for DRF project

--- quiz/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path, include
-from django.urls.resolvers import URLPattern
-from .views import helloAPI, randomQuiz
-
-urlpatterns = [
-    path('hello/', helloAPI),
-    path('<int:id>/', randomQuiz),
-]
