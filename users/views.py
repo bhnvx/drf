360a910235456cbc114a9eefcd15b2ae0f87474e
KyoungHwan <xzt______@naver.com>
2022-06-23 18:46:36 +0900

    user `token` retrieve viewset creating...

--- users/views.py
+++ users/views.py
@@ -1,6 +1,7 @@
-from rest_framework.viewsets import ModelViewSet
-from rest_framework import permissions
-from rest_framework.response import Response
+from rest_framework.viewsets import ModelViewSet, GenericViewSet
+from rest_framework import permissions, response
+from rest_framework.authtoken.models import Token
+from rest_framework import status
 
 from .models import Users
 from .serializers import UserSerializer, CustomRegisterSerializer
@@ -14,8 +15,8 @@ class UserViewSet(ModelViewSet):
     def perform_create(self, serializer):
         serializer.save(self.request)
 
-    def list(self, request, *args, **kwargs):
-        return super(UserViewSet, self).list(request, *args, **kwargs)
+    def retrieve(self, request, *args, **kwargs):
+        return super(UserViewSet, self).retrieve(request, *args, **kwargs)
 
     def create(self, request, *args, **kwargs):
         self.serializer_class = CustomRegisterSerializer
@@ -26,3 +27,18 @@ class UserViewSet(ModelViewSet):
 
     def destroy(self, request, *args, **kwargs):
         pass
+
+
+class UserGetTokenAndLoginViewSet(GenericViewSet):
+    serializer_class = None
+    permission_classes = [permissions.IsAuthenticated, ]
+    queryset = Users.objects.all()
+
+    def Login(self):
+        pass
+
+    def GetToken(self):
+        data = self.request.user.id
+        token = Token.objects.get(user_id=data)
+        res = {'token': token.key}
+        return response.Response(res, status=status.HTTP_200_OK)
