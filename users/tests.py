e2f8acffeef98bfa259a544b20d63ee070f6a0a9
KyoungHwan <xzt______@naver.com>
2022-07-18 11:14:30 +0900

    TDD concept

--- users/tests.py
+++ users/tests.py
@@ -6,4 +6,21 @@ class UserTestCase(TestCase):
     def setUp(self) -> None:
         super().setUp()
 
-    # ...
+        # User
+        self.AdminUser = User.objects.create(username="admin", is_staff=True, is_superuser=True)
+        self.User1 = User.objects.create(username="user1")
+        self.User2 = User.objects.create(username="user2")
+
+    def admin_test(self):
+        self.client.force_login(self.AdminUser)
+
+        res = self.client.post(
+            path="",
+            data={},
+            content_type=""
+        )
+        self.assertEqual(res.status_code, 201)
+        data = res.json()
+        self.assertIsNotNone(data["id"])
+
+        user = User.objects.get(id=data["id"])
