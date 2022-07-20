d5b968ec2488b5f14fc06a5b0e7f9c5a55806573
KyoungHwan <xzt______@naver.com>
2022-07-20 16:30:48 +0900

    `Get` method TDD Done

--- users/tests.py
+++ users/tests.py
@@ -15,15 +15,49 @@ class UserTestCase(TestCase):
         # URL Path
         self.url = "/api/v1/"
 
-    def test_admin_get_all_user(self):
+    def test_admin_get_user(self):
         self.client.force_login(self.AdminUser)
-        token = Token.objects.get(user_id=User.objects.get(username="admin").id)
+        token = Token.objects.get(user_id=self.AdminUser.id)
         headers = {'HTTP_AUTHORIZATION': f"Token {token}"}
 
         res = self.client.get(
             path="/api/v1/users/",
             **headers
         )
+
         self.assertEqual(res.status_code, 200)
         data = res.json()
         self.assertIsNotNone(data)
+        quesryset = User.objects.all()
+        self.assertEqual(len(data), len(quesryset))
+
+        for query in quesryset:
+            res = self.client.get(
+                path=f"/api/v1/users/{query.id}",
+                **headers
+            )
+            self.assertRedirects(res, f"/api/v1/users/{query.id}/", status_code=301, target_status_code=200)
+
+    def test_not_admin_get_user(self):
+        self.client.force_login(self.User1)
+        token = Token.objects.get(user_id=self.User1.id)
+        headers = {'HTTP_AUTHORIZATION': f"Token {token}"}
+
+        res = self.client.get(
+            path="/api/v1/users/",
+            **headers
+        )
+        self.assertEqual(res.status_code, 403)
+
+        pk = User.objects.get(id=self.User2.id).id
+        res = self.client.get(
+            path=f"/api/v1/users/{pk}",
+            **headers
+        )
+        self.assertRedirects(res, f"/api/v1/users/{pk}/", status_code=301, target_status_code=403)
+
+    def test_user_create(self):
+        res = self.client.post(
+            path="/api/v1/users/",
+            data={"username": None, "password1": None, "password2": None}
+        )
